9b6443fa0aa0dde879b601f07cf92cc07a72f879
hrs <hrs113355@gmail.com>
2016-06-07 14:27:00 +0800

    fix fetcher

--- lib/active_merchant/billing/integrations/pay2go_cvs/fetcher.rb
+++ lib/active_merchant/billing/integrations/pay2go_cvs/fetcher.rb
@@ -18,7 +18,7 @@ module ActiveMerchant #:nodoc:
           def fetch
             result = RestClient.post ActiveMerchant::Billing::Integrations::Pay2goCvs.gateway_url, { 
               MerchantID_: ActiveMerchant::Billing::Integrations::Pay2goCvs.merchant_id,
-              PostData_: encrypted_data(@params.to_query)
+              PostData_: self.encrypted_data(@params.to_query)
             }
             if @params['RespondType'] == 'JSON'
               JSON.parse(result)
@@ -30,22 +30,22 @@ module ActiveMerchant #:nodoc:
           private
 
           def pay2go_params(params)
-            params.permit(:RespondType, :TimeStamp, :Version, :MerchantOrderNo, :Amt, :ProdDesc, :AllowStore, :NotifyURL, :ExpireDate, :Email)
+            params.slice(:RespondType, :TimeStamp, :Version, :MerchantOrderNo, :Amt, :ProdDesc, :AllowStore, :NotifyURL, :ExpireDate, :Email)
           end
 
-          def padding(str, blocksize = 32)
+          def self.padding(str, blocksize = 32)
             len = str.size
             pad = blocksize - (len % blocksize)
             str += pad.chr * pad
           end
 
-          def encrypted_data(data)
+          def self.encrypted_data(data)
             cipher = OpenSSL::Cipher::AES.new(256, :CBC)
             cipher.encrypt
             cipher.padding = 0
             cipher.key = ActiveMerchant::Billing::Integrations::Pay2goCvs.hash_key
             cipher.iv = ActiveMerchant::Billing::Integrations::Pay2goCvs.hash_iv
-            data = padding(data)
+            data = self.padding(data)
             encrypted = cipher.update(data) + cipher.final
             encrypted.unpack('H*').first
           end
